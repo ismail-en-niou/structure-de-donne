67c6ff2e15e44f787a6bfe52f35ef8213a24ffcc
ismail en-niou <132809252+ismail-en-niou@users.noreply.github.com>
2024-05-09 15:18:25 +0100

    Your commit message here

---
+++ cour/les files/file.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+typedef struct 
+{
+    int tete;
+    int Queue;
+    int taille ;
+    int T[50];
+}file;
+int estVide( file f){
+    return (f.tete == f.Queue);
+}
+int estplen( file f ){
+    return (f.taille == f.Queue );
+}
+void EnFiler( file *f , int v ){
+    f->T[f->Queue++] = v;
+}
+int DeFiler(file *f){
+    return f->T[f->tete++] ;
+}
+int main(){
+    file f1 ; 
+    int n;
+    f1.Queue = f1.tete = 0 ;
+    printf("Donner la taille maximale de la file p1 : ");
+    scanf("%d", &f1.taille);
+
+
+    while (!estplen(f1))
+    {
+        printf("Donner la valeur que vous voulez ajouter : ");
+        scanf("%d", &n);
+        EnFiler(&f1, n);
+    }
+    while (!estVide(f1))
+    {
+        printf("%d \n", DeFiler(&f1));
+    }
+    return 0 ;
+}
